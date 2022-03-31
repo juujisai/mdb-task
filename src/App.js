f1500304bed8a7c3159d2b44f6af5b89faba996d
Bartosz <bartosz.cylwik94@gmail.com>
2022-03-31 13:38:35 +0200

    connect redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux'
+
+import { pcPartsReducer } from './redux/reducers/pcPartsReducer'
+
+const rootReducer = combineReducers({
+  pcParts: pcPartsReducer
+})
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
 
 function App() {
   return (
-    <div className="App">
-      <div></div>
-    </div>
+    <Provider store={store}>
+      <div className="App" >
+        <div></div>
+      </div>
+    </Provider>
   );
 }
 
---
+++ src/redux/reducers/pcPartsReducer.js
@@ -0,0 +1,11 @@
+
+
+const initialStore = {
+
+}
+
+
+export const pcPartsReducer = (state = initialStore, action) => {
+
+  return state
+}
