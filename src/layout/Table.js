a43829892daa4751d888f76a84da10c6b559639c
Bartosz <bartosz.cylwik94@gmail.com>
2022-04-01 14:13:24 +0200

    add edition of entry in table

--- src/layout/Table.js
+++ src/layout/Table.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { AiOutlineEdit, AiOutlineDelete } from 'react-icons/ai'
+import { BsSave } from 'react-icons/bs'
 import { setSumOfComponents, updateList } from '../redux/actions/pcPartsAction'
 
-const Table = ({ pcParts: { listOfComponents }, tools: { editActive }, addSumToReducer, update }) => {
+const Table = ({ pcParts: { listOfComponents, selectCategories }, tools: { editActive }, addSumToReducer, update }) => {
   // active tools state
   const [isEditActive, setIsEditActive] = React.useState(false)
   const [sum, setSum] = React.useState(0)
+  const [editedField, setEditedField] = React.useState('')
+
+  // state of edited item
+  const [itemName, setItemName] = React.useState('')
+  const [itemCompany, setItemCompany] = React.useState('')
+  const [itemModel, setItemModel] = React.useState('')
+  // const [itemCategory, setItemCategory] = React.useState(selectCategories[0])
+  const [itemCategory, setItemCategory] = React.useState('')
+  const [itemPrice, setItemPrice] = React.useState(0)
+
+  // copy of old data - that is now edited
+  let oldDataRef = React.useRef({})
+
+
 
   React.useEffect(() => {
     setIsEditActive(editActive)
@@ -21,9 +36,39 @@ const Table = ({ pcParts: { listOfComponents }, tools: { editActive }, addSumToR
   }, [listOfComponents, addSumToReducer])
 
   // console.log(isEditActive)
+  // console.log('renderuje table')
 
-  const handleEdit = (item) => {
-    console.log('edit')
+  const handleEdit = (item, isRowEdited) => {
+
+    if (editedField !== '') {
+      console.log('edit')
+      alert('najpierw zakończ edycję poprzedniego')
+      return console.log('najpierw zakończ edycję poprzedniego')
+    }
+
+    setEditedField(item)
+    setItemName(item.name)
+    setItemCompany(item.company)
+    setItemModel(item.model)
+    setItemCategory(item.category)
+    setItemPrice(item.price)
+    oldDataRef.current = item
+  }
+
+  const handleSave = () => {
+    console.log('zapisuję')
+    let copy = listOfComponents
+    const idOfItemThatChanges = copy.findIndex(item => item === oldDataRef.current)
+
+    const itemToPush = { name: itemName, company: itemCompany, model: itemModel, price: itemPrice, category: itemCategory }
+
+    copy.splice(idOfItemThatChanges, 1, itemToPush)
+
+    update(copy)
+    setEditedField('')
+
+
+
   }
 
   const handleDelete = (item) => {
@@ -36,21 +81,40 @@ const Table = ({ pcParts: { listOfComponents }, tools: { editActive }, addSumToR
   }
 
 
-  const tableContent = listOfComponents.map((item, id) => (
-    <tr className='table-section__tr' key={id}>
-      <td className="table-section__td">{id + 1}</td>
-      <td className="table-section__td">{item.name}</td>
-      <td className="table-section__td">{item.company}</td>
-      <td className="table-section__td">{item.model}</td>
-      <td className="table-section__td">{item.category}</td>
-      <td className="table-section__td">{Number(item.price)}</td>
-      {
-        isEditActive && <td className="table-section__td table-section__td--edit"><span className='edit-btn' onClick={() => handleEdit(item)}><AiOutlineEdit /></span><span className='edit-btn delete-tbl-btn' onClick={() => handleDelete(item)}><AiOutlineDelete /></span></td>
-      }
+  const tableContent = listOfComponents.map((item, id) => {
+    const isRowEdited = item === editedField
+
+    return (
+      <tr className='table-section__tr' key={id}>
+        <td className="table-section__td">{id + 1}</td>
+        <td className="table-section__td">
+          <input className='table-section__input' type="text" value={isRowEdited ? itemName : item.name} disabled={!isRowEdited} onChange={(e) => setItemName(e.target.value)} />
+        </td>
+        <td className="table-section__td">
+          <input className='table-section__input' type="text" value={isRowEdited ? itemCompany : item.company} disabled={!isRowEdited} onChange={(e) => setItemCompany(e.target.value)} />
+        </td>
+        <td className="table-section__td">
+          <input className='table-section__input' type="text" value={isRowEdited ? itemModel : item.model} disabled={!isRowEdited} onChange={(e) => setItemModel(e.target.value)} />
+        </td>
+        <td className="table-section__td">
+          <input className='table-section__input' type="text" value={isRowEdited ? itemCategory : item.category} disabled={!isRowEdited} onChange={(e) => setItemCategory(e.target.value)} />
+        </td>
+        <td className="table-section__td">
+          <input className='table-section__input' type="number" min='0' value={Number(isRowEdited ? itemPrice : item.price)} disabled={!isRowEdited} onChange={(e) => setItemPrice(e.target.value)} />
+        </td>
+        {
+          isEditActive && <td className="table-section__td table-section__td--edit">
+            {!isRowEdited ? <span className='edit-btn' onClick={() => handleEdit(item, isRowEdited)}><AiOutlineEdit /></span>
+              : <span className='edit-btn' onClick={() => handleSave()}><BsSave /></span>}
+
+            <span className='edit-btn delete-tbl-btn' onClick={() => handleDelete(item)}><AiOutlineDelete /></span>
+          </td>
+        }
 
 
-    </tr>
-  ))
+      </tr>
+    )
+  })
 
 
 
